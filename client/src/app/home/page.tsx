1a6936af8957a6ee2d8090ea28e569881c676c8c
nbpillai <nipillai@ucsd.edu>
2024-04-20 02:42:07 -0700

    landing mostly done

--- client/src/app/home/page.tsx
+++ client/src/app/home/page.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 import styles from "@/app/home/page.module.css";
 import NavBar from "@/components/NavBar";
+import { constrainedMemory } from "process";
 
 export default function Landing() {
   return (
-    <main className={styles.page}>
+    <main
+      className={styles.page}
+      style={{
+        backgroundImage: `url(${"./LandingHero.svg"})`,
+        backgroundSize: "contain",
+        width: "100%",
+        height: "100%",
+      }}
+    >
       <>
         <div className={styles.container}>
           <NavBar />
