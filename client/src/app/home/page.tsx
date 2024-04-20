3cf8d756ba1e91f5a22b7107b08acb0dd6986992
nbpillai <nipillai@ucsd.edu>
2024-04-20 02:25:56 -0700

    landing page started

--- client/src/app/home/page.tsx
+++ client/src/app/home/page.tsx
@@ -1,7 +1,21 @@
+import React from "react";
+import styles from "@/app/home/page.module.css";
+import NavBar from "@/components/NavBar";
+
 export default function Landing() {
   return (
-    <div>
-      <p>landing</p>
-    </div>
+    <main className={styles.page}>
+      <>
+        <div className={styles.container}>
+          <NavBar />
+          <div className={styles.hero}>
+            <div className={styles.header}>Welcome to superswagproject</div>
+            <button className={styles.cookButton}>
+              Get Cooking &nbsp; &rarr;
+            </button>
+          </div>
+        </div>
+      </>
+    </main>
   );
 }
