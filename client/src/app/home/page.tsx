512885e03fc3ca46074bf45658314b03b0fa5f19
nbpillai <nipillai@ucsd.edu>
2024-04-20 16:19:11 -0700

    fixed svg

--- client/src/app/home/page.tsx
+++ client/src/app/home/page.tsx
@@ -6,23 +6,38 @@ export default function Landing() {
   return (
     <main
       className={styles.page}
-      style={{
-        backgroundImage: `url(${"./LandingHero.svg"})`,
-        backgroundSize: "cover",
-        backgroundRepeat: "no-repeat",
-        width: "100%",
-        height: "100%",
-      }}
+      // style={{
+      //   backgroundImage: `url(${"./LandingHero.svg"})`,
+      //   backgroundSize: "cover",
+      //   backgroundRepeat: "no-repeat",
+      //   width: "100%",
+      //   height: "100%",
+      // }}
     >
       <>
-        <div className={styles.container}>
-          <NavBar />
-          <div className={styles.hero}>
-            <div className={styles.header}>Welcome to superswagproject</div>
-            <button className={styles.cookButton}>
-              Get Cooking &nbsp; &rarr;
-            </button>
+        <div
+          className={styles.overlay}
+          style={{
+            backgroundImage: `url(${"./LandingHero.svg"})`,
+            backgroundSize: "cover",
+            // backgroundRepeat: "no-repeat",
+            width: "100%",
+            height: "100%",
+          }}
+        >
+          <div className={styles.container}>
+            <NavBar />
+            <div className={styles.hero}>
+              <div className={styles.header}>Welcome to superswagproject</div>
+              <button className={styles.cookButton}>
+                Get Cooking &nbsp; &rarr;
+              </button>
+            </div>
           </div>
+        </div>
+        <div className={styles.missionCol}>
+          Generate a recipe using ingredients you already have in your fridge
+          and pantry to reduce food waste
         </div>
       </>
     </main>
