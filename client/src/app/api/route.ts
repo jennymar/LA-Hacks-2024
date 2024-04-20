6b323b4e70b58e36f8f43afeb51c3a96879c810f
jennymar <jenmar528@gmail.com>
2024-04-20 12:29:12 -0700

    Recipe response separated into diff vars

--- client/src/app/api/route.ts
+++ client/src/app/api/route.ts
@@ -1,5 +1,8 @@
 import { StreamingTextResponse, GoogleGenerativeAIStream } from "ai";
-import { GoogleGenerativeAI } from "@google/generative-ai";
+import {
+  GenerateContentResult,
+  GoogleGenerativeAI,
+} from "@google/generative-ai";
 
 export async function POST(req: Request, res: Response) {
   const reqBody = await req.json();
@@ -7,21 +10,17 @@ export async function POST(req: Request, res: Response) {
 
   var prompt = "";
 
-  if (reqBody.data.type === "ingredients") {
-    prompt =
-      "Please return JSON describing the title, time, ingredients, and steps from a recipe made from the following ingredients: " +
-      reqBody.data.prompt +
-      '. {"title": str, "time": str, "ingredients": list[INGREDIENT], "steps": list[STEP]} INGREDIENT = {"name": str} STEP = {"description": str}';
-    //"I have a list of ingredients and I want to make a recipe from the following: " +
-    //reqBody.data.prompt +
-    //". Give me a recipe using these ingredients and show me the steps and time it would take.  ";
-  } else {
-    prompt = "";
-  }
+  prompt =
+    "Please return JSON describing the title, time, ingredients, and steps from a recipe made from the following ingredients: " +
+    reqBody.data.prompt +
+    '. Please be in depth in your steps. For each of the ingredients listed, specify the amount and unit to be used. Use the following schema: {"title": str, "time": str, "ingredients": list[INGREDIENT], "steps": list[STEP]}\n INGREDIENT = {"amountofunit": str, "ingredient": str} \n STEP = {"description": str} \n All fields are required. Important: Only returna single piece of valid JSON text.';
+  //"I have a list of ingredients and I want to make a recipe from the following: " +
+  //reqBody.data.prompt +
+  //". Give me a recipe using these ingredients and show me the steps and time it would take.  ";
 
   const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY!);
   const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+
   const streamingResponse = await model.generateContentStream(prompt);
-  // console.log("response: ", streamingResponse);
   return new StreamingTextResponse(GoogleGenerativeAIStream(streamingResponse));
 }
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -49,45 +49,40 @@ export default function Home() {
   }
 
   function OutputResponse() {
-    return (
-      <div
-        id="chatbox"
-        className="flex flex-col-reverse w-full text-left mt-4 gap-4 whitespace-pre-wrap"
-      >
-        {messages.map((message, index) => (
-          <div
-            key={index}
-            className={`p-4 shadow-md rounded-md ml-10 relative ${
-              message.role === "user" ? "bg-stone-300" : ""
-            }`}
-          >
-            {message.content}
-          </div>
-        ))}
-
-        {/* {messages.map((message, index) => {
-          // Parse the content of the message
-          console.log("message.content: ", message.content);
-
-          // Check if the role of the message is "assistant"
-          if (message.role === "assistant") {
-            // Render the title if the role is "assistant"
-
-            const parsedContent = JSON.parse(message.content);
-            return (
-              <div
-                key={index}
-                className={`p-4 shadow-md rounded-md ml-10 relative`}
-              >
-                {parsedContent.title}
-              </div>
-            );
-          } else {
-            // Return null if the role is "user" to skip rendering this message
-            return null;
-          }
-        })} */}
-      </div>
-    );
+    console.log("messages: ", messages);
+    if (
+      messages &&
+      messages[1] &&
+      messages[1].content.substring(messages[1].content.length - 2) === "]}"
+    ) {
+      const parsedContent = JSON.parse(messages[1].content);
+      const SPACE = " ";
+      console.log("parsedContent.title: ", parsedContent.title);
+      return (
+        <div
+          id="chatbox"
+          className="flex flex-col w-full text-left mt-4 gap-4 whitespace-pre-wrap"
+        >
+          <div> Title: {parsedContent.title}</div>
+          <div> Time: {parsedContent.time}</div>
+          <div> Ingredients: </div>
+          <ul>
+            {parsedContent.ingredients.map((ingredient: any, index: number) => (
+              <li key={index}>
+                {ingredient.amountofunit}
+                {SPACE}
+                {ingredient.ingredient}
+              </li>
+            ))}
+          </ul>
+          <div> Steps: </div>
+          <ol>
+            {parsedContent.steps.map((step: any, index: number) => (
+              <li key={index}>{step.description}</li>
+            ))}
+          </ol>
+        </div>
+      );
+    }
   }
 }
