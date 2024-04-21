5838e3ba303ea3bb6aab96a883ba615e04f64b1d
jennymar <jenmar528@gmail.com>
2024-04-20 20:31:38 -0700

    Textfields all get inputted to gemini

--- client/src/app/api/route.ts
+++ client/src/app/api/route.ts
@@ -5,22 +5,34 @@ import {
 } from "@google/generative-ai";
 
 export async function POST(req: Request, res: Response) {
+  console.log("POST");
   const reqBody = await req.json();
-  console.log(reqBody);
+  console.log("reqBody: ", reqBody);
 
   var prompt = "";
 
   prompt =
     "Please return JSON describing the title, time, ingredients, and steps from a recipe made from the following ingredients: " +
     reqBody.data.prompt +
+    ". I have the time frame of " +
+    reqBody.data.hr +
+    " hours " +
+    reqBody.data.min +
+    " min or time. I also have the dietary restrictions of " +
+    reqBody.data.dietRes +
+    ". " +
     '. Please be in depth in your steps. For each of the ingredients listed, specify the amount and unit to be used. Use the following schema: {"title": str, "time": str, "ingredients": list[INGREDIENT], "steps": list[STEP]}\n INGREDIENT = {"amountofunit": str, "ingredient": str} \n STEP = {"description": str} \n All fields are required. Important: Only returna single piece of valid JSON text.';
   //"I have a list of ingredients and I want to make a recipe from the following: " +
   //reqBody.data.prompt +
   //". Give me a recipe using these ingredients and show me the steps and time it would take.  ";
 
+  console.log("prompt: ", prompt);
   const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY!);
   const model = genAI.getGenerativeModel({ model: "gemini-pro" });
 
+  console.log("after model");
+
   const streamingResponse = await model.generateContentStream(prompt);
+  console.log("response");
   return new StreamingTextResponse(GoogleGenerativeAIStream(streamingResponse));
 }
--- client/src/app/cook/page.tsx
+++ client/src/app/cook/page.tsx
@@ -1,51 +1,164 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import styles from "@/app/cook/page.module.css";
 import NavBar from "@/components/NavBar";
+import { useChat } from "ai/react";
 
 export default function Cook() {
+  const { messages, input, handleInputChange, handleSubmit, isLoading, stop } =
+    useChat({
+      api: "api",
+    });
+
   return (
-    <main className={styles.page}>
-      <>
-        <div className={styles.container}>
-          <NavBar />
-          <div className={styles.detailsContainer}>
-            <div className={styles.header}>Let's get cooking...</div>
-            <div className={styles.detailsRow}>
-              <div className={styles.detailsCol}>
-                <div className={styles.detailsSectionCol}>
-                  <div className={styles.detailsLabel}>Ingredients List</div>
-                  <input className={styles.ingredients} />
-                </div>
-              </div>
-              <div className={styles.detailsCol}>
-                <div className={styles.detailsSectionCol}>
-                  <div className={styles.detailsLabel}>Meal Type:</div>
-                  <div className={styles.mealRow}>
-                    <button className={styles.mealButton}>Breakfast</button>
-                    <button className={styles.mealButton}>Lunch</button>
-                    <button className={styles.mealButton}>Dinner</button>
-                    <button className={styles.mealButton}>Snack</button>
-                    <button className={styles.mealButton}>Dessert</button>
-                  </div>
-                </div>
-                <div className={styles.detailsSectionCol}>
-                  <div className={styles.detailsLabel}>Time:</div>
-                  <div className={styles.timeRow}>
-                    <input className={styles.timeInput} />
-                    <input className={styles.timeInput} />
-                  </div>
-                </div>
-                <div className={styles.detailsSectionCol}>
-                  <div className={styles.detailsLabel}>
-                    Dietary Restrictions:
-                  </div>
-                  <input className={styles.dietInput} />
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </>
+    <main>
+      {InputForm()}
+      {OutputResponse()}
     </main>
   );
+
+  function InputForm() {
+    const [ingredients, setIngredients] = useState<string>();
+    const [hour, setHour] = useState<string>();
+    const [min, setMin] = useState<string>();
+    const [dietRes, setDietRes] = useState<string>();
+
+    useEffect(() => {
+      console.log("ingredients: ", ingredients);
+      console.log("hour: ", hour);
+      console.log("min: ", min);
+      console.log("dietRes: ", dietRes);
+    });
+
+    return (
+      <main className={styles.page}>
+        <>
+          <form
+            onSubmit={(event) => {
+              console.log("onSubmit");
+              event.preventDefault();
+              handleSubmit(event, {
+                data: {
+                  prompt: input,
+                  ingredients: ingredients as string,
+                  hr: hour as string,
+                  min: min as string,
+                  dietRes: dietRes as string,
+                },
+              });
+            }}
+          >
+            <div className={styles.container}>
+              <NavBar />
+              <div className={styles.detailsContainer}>
+                <div className={styles.header}>Let's get cooking...</div>
+                <div className={styles.detailsRow}>
+                  <div className={styles.detailsCol}>
+                    <div className={styles.detailsSectionCol}>
+                      <div className={styles.detailsLabel}>
+                        Ingredients List
+                      </div>
+                      <textarea
+                        className={styles.ingredients}
+                        placeholder="What do you have in your pantry..."
+                        value={input}
+                        onChange={handleInputChange}
+                      />
+                    </div>
+                  </div>
+                  <div className={styles.detailsCol}>
+                    <div className={styles.detailsSectionCol}>
+                      <div className={styles.detailsLabel}>Meal Type:</div>
+                      <div className={styles.mealRow}>
+                        <button className={styles.mealButton}>Breakfast</button>
+                        <button className={styles.mealButton}>Lunch</button>
+                        <button className={styles.mealButton}>Dinner</button>
+                        <button className={styles.mealButton}>Snack</button>
+                        <button className={styles.mealButton}>Dessert</button>
+                      </div>
+                    </div>
+                    <div className={styles.detailsSectionCol}>
+                      <div className={styles.detailsLabel}>Time:</div>
+                      <div className={styles.timeRow}>
+                        <input
+                          className={styles.timeInput}
+                          placeholder="hr"
+                          value={hour}
+                          onChange={(e) => setHour(e.target.value)}
+                        />
+                        <input
+                          className={styles.timeInput}
+                          placeholder="min"
+                          value={min}
+                          onChange={(e) => setMin(e.target.value)}
+                        />
+                      </div>
+                    </div>
+                    <div className={styles.detailsSectionCol}>
+                      <div className={styles.detailsLabel}>
+                        Dietary Restrictions:
+                      </div>
+
+                      <textarea
+                        className={styles.dietInput}
+                        placeholder="Do you have any dietary restrictions..."
+                        value={dietRes}
+                        onChange={(e) => setDietRes(e.target.value)}
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className={styles.buttonRow}>
+                <button className={styles.backButton}>
+                  &larr; &nbsp; Back
+                </button>
+                <button className={styles.submitButton} onClick={stop}>
+                  Submit
+                </button>
+              </div>
+            </div>
+          </form>
+        </>
+      </main>
+    );
+  }
+
+  function OutputResponse() {
+    console.log("messages: ", messages);
+    if (
+      messages &&
+      messages[1] &&
+      messages[1].content.substring(messages[1].content.length - 2) === "]}"
+    ) {
+      const parsedContent = JSON.parse(messages[1].content);
+      const SPACE = " ";
+      console.log("parsedContent.title: ", parsedContent.title);
+      return (
+        <div
+          id="chatbox"
+          className="flex flex-col w-full text-left mt-4 gap-4 whitespace-pre-wrap"
+        >
+          <div> Title: {parsedContent.title}</div>
+          <div> Time: {parsedContent.time}</div>
+          <div> Ingredients: </div>
+          <ul>
+            {parsedContent.ingredients.map((ingredient: any, index: number) => (
+              <li key={index}>
+                {ingredient.amountofunit}
+                {SPACE}
+                {ingredient.ingredient}
+              </li>
+            ))}
+          </ul>
+          <div> Steps: </div>
+          <ol>
+            {parsedContent.steps.map((step: any, index: number) => (
+              <li key={index}>{step.description}</li>
+            ))}
+          </ol>
+        </div>
+      );
+    }
+  }
 }
